2a8ea9ac41720ba5214d841961067381cc5f4382
okazaki_daiki <okazaki_daiki@localhost.localdomain>
2015-11-30 19:02:24 +0900

    HPとエネルギーゲージの追加

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+// === サーバに関する処理 ===
 	// 接続先の指定(192.168.8.89)
 	var url = "http://192.168.8.89:8080";
 	// 接続
@@ -27,9 +28,9 @@ window.onload = function() {
 
 	var myName = "User" + Math.floor(Math.random()*10000);
 	start(myName)
-
-// === ここまでサーバに向けた処理 ===
 	
+
+// === ゲームに関する処理 ===
     var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 	function preload () {
@@ -37,60 +38,105 @@ window.onload = function() {
 		game.load.image('player', 'asset/player.png');
 	}
 
-	var player;
-	var enemy;
-	var SPEED = 100;
+	var player, enemy, energy_bar, hp_bar;
 	var ANGLE = 200;
+	var BOOST_POWER = 5000;	
+	var USE_ENERGY = 20;	
     function create () {
 		// 物理計算方式
 		game.physics.startSystem(Phaser.Physics.P2JS);
 		
 		// キーボードのインプットを登録
-		cursors = game.input.keyboard.createCursorKeys();
-		
+		keys = game.input.keyboard.createCursorKeys();
+
 		// スプライトを生成
 		enemy = game.add.sprite(200, 300, 'enemy');
+		enemy.scale.setTo(0.3, 0.3);
 		// enemyにphyisicsを付与
 		game.physics.p2.enable(enemy);
 		// あたり判定 ( 四角形 )
-		enemy.body.setRectangle(40, 40);
+		enemy.body.setRectangle(20, 80);
 		
 		// playerの設定
 		player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
 		game.physics.p2.enable(player);
+		player.scale.setTo(0.3, 0.3);
 		player.anchor.setTo(0.5, 0.5);
+		player.body.setRectangle(20, 80);
+		// プレイヤーの移動処理
+		keys.up.onDown.add(player_move, this);
+		// プレイヤーの回復処理
+		game.time.events.loop(Phaser.Timer.QUARTER, player_recovery, this);
 		// あたり判定 ( 円 )
-		player.body.setCircle(28);
+		// player.body.setCircle(25);
+		
 		// 最大HP
 		console.log(player.maxHealth);
 		// HPの回復
-		player.heal(10);
+		player.heal(50);
 		// 現在のHP
 		console.log(player.health);
-		// 名前1
+		
+		// HPゲージ
+		var hp_bar_bg = game.add.graphics(50, 30);
+		hp_bar_bg.lineStyle(16, 0xff0000, 0.8);
+		hp_bar_bg.lineTo(player.maxHealth, 0);
+		hp_bar = game.add.graphics(50, 30);
+
+		// power 最大値
+		player.maxEnergy = 100;
+		// power 初期値
+		player.energy = 100;
+		
+		// powerゲージ
+		var energy_bar_bg = game.add.graphics(80, 52.5);
+		energy_bar_bg.lineStyle(16, 0xff0000, 0.8);
+		energy_bar_bg.lineTo(player.maxEnergy, 0);
+		energy_bar = game.add.graphics(80, 52.5);
+		
+		// 名前
 		player.name = myName;
 		console.log(player.name);
-    }
 
-	function update() {
+		// テキスト
+		text = game.add.text(20, 20, "HP: \n" + "Energy: ", { font: "16px Arial", fill: "#EEE" });
+		// ゲーム内時間
+		game.time.events.loop(Phaser.Timer.SECOND, game_time, this);
+	}
 
-		player.body.velocity.x = 0;
-		player.body.velocity.y = 0;
-		player.body.angularVelocity = 0;
-		
-		if (cursors.up.isDown){
-			// 角度を-90することで上に向かって移動する
-			player.body.thrust(10000);
+	// プレイヤーメソッド
+	function player_move(){
+		if (player.energy - USE_ENERGY >= 0 ){
+			player.energy -= USE_ENERGY;
+			player.body.thrust(BOOST_POWER);
 		}
+	}
 
-		player.body.angularAcceleration = 0;
-		if (cursors.left.isDown){
+	function player_recovery(){
+	    // Energyの回復
+		if(player.energy < player.maxEnergy){
+			player.energy++;
+		}
+		if(player.health < player.maxHealth){
+			player.health += 0.2;
+		}
+    }
+	
+	function update() {
+		player.body.angularVelocity = 0;
+		energy_bar.clear().moveTo(0,0).lineStyle(16, 0x00ced1, 0.9).lineTo(player.energy, 0);
+		hp_bar.clear().moveTo(0,0).lineStyle(16, 0x00ff00, 0.9).lineTo(player.health, 0);
+
+		if (keys.left.isDown){
 			player.body.rotateLeft(100);
-		}else if (cursors.right.isDown){
+		}else if (keys.right.isDown){
 			player.body.rotateRight(100);
 		}
+		player.body.angularAcceleration = 0;
+	}
 
-		game.physics.arcade.collide(enemy, player, collisionHandler, null, this);
+	// 一秒ごとの処理
+	function game_time() {
 	}
 	
 	// イベントハンドラ（コールバック関数）
